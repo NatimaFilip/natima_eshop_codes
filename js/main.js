f36eaa947ab20c8c374894e8bcb48ad805c9363f
NatimaFilip <filip.h@natima.cz>
2026-07-10 10:21:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1170,26 +1170,6 @@ if (body.classList.contains("type-category")) {
 		}
 	});
 
-	document.addEventListener("RAVENTIC SEARCH RESULTS TRANSFORMED", function (event) {
-		const customOpenFilterButtonRaventic = document.createElement("a");
-		customOpenFilterButtonRaventic.className = "custom-open-filter-button";
-		customOpenFilterButtonRaventic.innerHTML = translationsStrings.customFilterButton[activeLang];
-
-		try {
-			const raventicResultsTop = document.querySelector(".raventic-search-results-container");
-			raventicResultsTop.appendChild(customOpenFilterButtonRaventic);
-
-			customOpenFilterButtonRaventic.addEventListener("click", function () {
-				const raventicFilters = document.querySelector(".raventic-search-results-filters");
-				if (raventicFilters) {
-					raventicFilters.classList.toggle("active");
-				}
-			});
-		} catch (error) {
-			console.error("Error in customOpenFilterButtonListener:", error);
-		}
-	});
-
 	function moveAsideToCategoryContent() {
 		if (asideElement) {
 			categoryContentWrapper.prepend(asideElement);
@@ -6527,3 +6507,23 @@ function clickOutsideSearchDropdown() {
 		}
 	});
 }
+
+document.addEventListener("RAVENTIC SEARCH RESULTS TRANSFORMED", function (event) {
+	const customOpenFilterButtonRaventic = document.createElement("a");
+	customOpenFilterButtonRaventic.className = "custom-open-filter-button";
+	customOpenFilterButtonRaventic.innerHTML = translationsStrings.customFilterButton[activeLang];
+
+	try {
+		const raventicResultsTop = document.querySelector(".raventic-search-results-container");
+		raventicResultsTop.appendChild(customOpenFilterButtonRaventic);
+
+		customOpenFilterButtonRaventic.addEventListener("click", function () {
+			const raventicFilters = document.querySelector(".raventic-search-results-filters");
+			if (raventicFilters) {
+				raventicFilters.classList.toggle("active");
+			}
+		});
+	} catch (error) {
+		console.error("Error in customOpenFilterButtonListener:", error);
+	}
+});
